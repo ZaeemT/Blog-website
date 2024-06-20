6b8edb21fec47852fe1e12ccbf5431e23a46ab4a
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-06-20 22:07:22 +0500

    Made custom hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,10 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "../hooks/useFetch";
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (res != ok){     // This is for if there is fault in response while the server or network is fine.
-                    throw Error('Could not fetch data for that resource.');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {     // This for displaying network error message.
-                isPending(false);
-                setError(err.message);
-            });
-    }, []);
-
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+    
 
     return (
         <div className="home">
