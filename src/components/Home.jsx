9f8e6847087d89b0acb2aa54c14436b212b0ef81
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-06-20 21:33:42 +0500

    Handling errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,34 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (res != ok){     // This is for if there is fault in response while the server or network is fine.
+                    throw Error('Could not fetch data for that resource.');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {     // This for displaying network error message.
+                isPending(false);
+                setError(err.message);
             });
     }, []);
 
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading....</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
-
         </div>
     );
 }
